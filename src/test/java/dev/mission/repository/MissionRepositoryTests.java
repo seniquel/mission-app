c3b5b43bef9d66a654ef699e42b7be861e6bd9ae
Léo SENIQUE <leo.senique@gmail.com>
2020-07-20 00:14:21 +0200

    ajout tests corrects

--- src/test/java/dev/mission/repository/MissionRepositoryTests.java
+++ src/test/java/dev/mission/repository/MissionRepositoryTests.java
@@ -4,12 +4,12 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.util.List;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
-import org.springframework.transaction.annotation.Transactional;
 
 import dev.mission.entite.Mission;
 
@@ -37,13 +37,39 @@ public class MissionRepositoryTests {
 		
 		entityManager.getEntityManager().persist(mission);
 		entityManager.getEntityManager().persist(mission2);
-		assertThat(missionRepository.findAll()).contains(mission);
-		assertThat(missionRepository.findAll()).doesNotContain(mission2);
+		List<Mission> missions = missionRepository.findMissionByDate();
+		assertThat(missions).contains(mission);
+		assertThat(missions).doesNotContain(mission2);
 	}
 	@Test
 	void findByDateDebutGreaterThanEqualAndTauxJournalierGreaterThanEqual() {
-		// TODO insérer des données avec `entityManager`
-		// TODO exécuter la requête
-		// TODO vérifier le résultat
+		//Mission valide
+		Mission mission = new Mission();
+		mission.setLibelle("Mission 1");
+		mission.setTauxJournalier(new BigDecimal("100"));
+		mission.setDateDebut(LocalDate.of(2500, 1, 1));
+		mission.setDateFin(LocalDate.of(2500, 1, 2));
+		
+		//Mission avec date invalide
+		Mission mission2 = new Mission();
+		mission2.setLibelle("Mission 2");
+		mission2.setTauxJournalier(new BigDecimal("100"));
+		mission2.setDateDebut(LocalDate.of(1500, 1, 1));
+		mission2.setDateFin(LocalDate.of(1500, 1, 2));
+		
+		//Mission avec taux journalier invalide
+		Mission mission3 = new Mission();
+		mission3.setLibelle("Mission 2");
+		mission3.setTauxJournalier(new BigDecimal("50"));
+		mission3.setDateDebut(LocalDate.of(2500, 1, 1));
+		mission3.setDateFin(LocalDate.of(2500, 1, 2));
+		
+		entityManager.getEntityManager().persist(mission);
+		entityManager.getEntityManager().persist(mission2);
+		entityManager.getEntityManager().persist(mission3);
+		List<Mission> missions = missionRepository.findMissionByDateAndTaux(new BigDecimal(100));
+		assertThat(missions).contains(mission);
+		assertThat(missions).doesNotContain(mission2);
+		assertThat(missions).doesNotContain(mission3);
 	}
 }
